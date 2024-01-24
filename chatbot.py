5b5b84d0493471491a134263e29f3a2ea585aeb3
Tejasvi Jatic <121444242+tejasviii21@users.noreply.github.com>
2024-01-25 00:10:08 +0530

    update backend

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
 
-app = Flask(__name__, static_folder='frontend/build', static_url_path='/')
+
+app = Flask(__name__, static_folder='build', static_url_path='/')
 CORS(app)  # Enable CORS for all routes
 
 # Configure OpenAI API credentials
@@ -10,10 +11,9 @@ openai.api_key = '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'
 
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
-    # print("saini")
     data = request.get_json()
     message = data['message']
-    print(message)
+    # print(message)
     # prompt = f"I have a fashion-related question: {message}"
     
     # Make a request to OpenAI API for the chatbot response
@@ -32,17 +32,24 @@ def chatbot():
     # answer = response.choices[0].text.strip()
     from openai import OpenAI
 
-    client = OpenAI(api_key = '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')
+    client = OpenAI(api_key = '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')
 
     stream = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": message}],
         stream=True,
     )
+    # print(stream)
+    # for chunk in stream:
+    #     if chunk.choices[0].delta.content is not None:
+    #         print(chunk.choices[0].delta.content, end="")
+    # return jsonify({'response': chunk.choices[0].delta.content})
+    response_content = ""
     for chunk in stream:
         if chunk.choices[0].delta.content is not None:
-            print(chunk.choices[0].delta.content, end="")
-    return jsonify({'response': chunk.choices[0].delta.content})
+            response_content += chunk.choices[0].delta.content
+
+    return jsonify({'response': response_content})
 
 @app.route('/')
 def serve_frontend():
